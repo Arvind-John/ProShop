d932107d3ea877f628133f02bbac3665e2506e9c
Arvind-John <arvindjohn135@gmail.com>
2024-09-26 20:25:24 +0530

    Final Commit

--- backend/server.js
+++ backend/server.js
@@ -29,12 +29,14 @@ app.get("/api/config/paypal", (req, res) =>
 );
 
 if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
+  const __dirname = path.resolve();
   app.get("/", (req, res) => {
     res.send("API is running...");
   });
